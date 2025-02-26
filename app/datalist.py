2ebf3280e4a83284bc791b0524b7d06522823408
kazookie <kzki@hacklab.jp>
2025-02-26 23:13:39 +0900

    changed to data loading using thread

--- app/datalist.py
+++ app/datalist.py
@@ -1,23 +1,18 @@
 import os
 import glob
 import json
-from PySide6.QtCore import Signal
+from PySide6.QtCore import Signal, QThread
 from PySide6.QtWidgets import QListWidget, QListWidgetItem, QAbstractItemView
 
-class DataListWidget(QListWidget):
-    changed = Signal(object)
-
-    def __init__(self, main_window):
+class DataLoadWorker(QThread):
+    def __init__(self, widget):
         super().__init__()
-        self.main_window = main_window
-
-        self.create_file_list()
-
-    def create_file_list(self):
+        self.widget = widget
+    
+    def run(self):
         data_dir = os.getenv("APPDATA") + "\\Beat Savior Data\\"
         files = reversed(glob.glob(data_dir + "\\20*"))
 
-        self.play_datas = []
         for file in files:
             # Read file
             fp = open(file, 'r', encoding='utf-8')
@@ -26,12 +21,27 @@ class DataListWidget(QListWidget):
 
             for line in lines[1:-1]:
                 data = json.loads(line)
-                self.play_datas.append(data)
+                self.widget.play_datas.append(data)
                 item = QListWidgetItem()
                 item_text = data['songName'] + ' by ' + data['songArtist'] + ' Mapped by '+ data['songMapper']
-                item.setText(data['songName'] + ' by ' + data['songArtist'] + ' Mapped by '+ data['songMapper'])
+                item.setText(item_text)
                 item.setToolTip(item_text)
-                self.addItem(item)
+                self.widget.addItem(item)
+
+
+
+class DataListWidget(QListWidget):
+    changed = Signal(object)
+
+    def __init__(self, main_window):
+        super().__init__()
+        self.main_window = main_window
+        self.create_file_list()
+
+    def create_file_list(self):
+        self.play_datas = []
+        self.dataloadworker = DataLoadWorker(self)
+        self.dataloadworker.start()
     
     def currentChanged(self, current, previous):
         index = current.row()
